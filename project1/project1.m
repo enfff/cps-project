15fb59227e99bd7853fd59b69c7b1e7a3c56f628
SimoneCarena <simonecarena2000@gmail.com>
2023-03-21 20:59:06 +0100

    completed observations of mean,max,min convergence time and number of misevaluations in function of q. Also added respective plots

--- project1/project1.m
+++ project1/project1.m
@@ -8,57 +8,105 @@ clc;
 
 format long
 
-miss = 0;
-outer_iterations = 20;
-iterations = zeros(outer_iterations,1);
-q = 10; %dimension of y
-lambda = 1e-1;
-p = 20; %dimension of x
-L = lambda*ones(p,1); %vector of lambda 
-epsilon = 1e-8; %precision (?)
-sigma = 1e-2; %standard deviation
-nu = sigma^(2)*randn(q,1);
-delta = 1e-12;
-k=2; %numeber of non-null elements of x
+misses = [];
+means = [];
+maxes = [];
+mins = [];
 
-for i=1:outer_iterations
+Q = 10:50;
+for q=Q
+    miss = 0;
+    outer_iterations = 20;
+    iterations = zeros(outer_iterations,1);
+    lambda = 1e-1;
+    p = 20; %dimension of x
+    L = lambda*ones(p,1); %vector of lambda 
+    epsilon = 1e-8; %precision (?)
+    sigma = 1e-2; %standard deviation
+    nu = sigma^(2)*randn(q,1);
+    delta = 1e-12;
+    k=2; %numeber of non-null elements of x
 
-    C = randn(q,p);
-    tau = norm(C,2)^(-2) - epsilon;
-    x = 1 + (2-1)*rand(p,1);
-    x_0 = zeros(p,1);
+    for i=1:outer_iterations
+        C = randn(q,p);
+        tau = norm(C,2)^(-2) - epsilon;
+        x = 1 + (2-1)*rand(p,1);
+        x_0 = zeros(p,1);
 
-    mask = randi([1 p],k,1); %generates k non-null indices
+        mask = randi([1 p],k,1); %generates k non-null indices
 
-    for j=1:p
-        if ~ismember(mask,j)
-            x(j) = 0;
+        for j=1:p
+            if ~ismember(mask,j)
+                x(j) = 0;
+            end
         end
-    end
 
-    y = C*x + nu;
-    [x_zero_norm, x_indices] = zero_norm(x);
+        y = C*x + nu;
+        [x_zero_norm, x_indices] = zero_norm(x);
 
-    while 1
-        iterations(i) = iterations(i)+1;
-        iterations(i)=iterations(i)+1;
-        x_new = IST(x_0+tau*C'*(y-C*x_0),L);
-        if norm(x_new - x_0,2)<delta
-            break;
+        while 1
+            iterations(i) = iterations(i)+1;
+            iterations(i)=iterations(i)+1;
+            x_new = IST(x_0+tau*C'*(y-C*x_0),L);
+            if norm(x_new - x_0,2)<delta
+                break;
+            end
+            x_0 = x_new;
         end
-        x_0 = x_new;
+    
+        [x0_zero_norm, x0_indices] = zero_norm(x_0);
+        if ~compare(x0_indices,x_indices)
+            miss = miss+1;
+        end
     end
     
-    [x0_zero_norm, x0_indices] = zero_norm(x_0);
-    if ~compare(x0_indices,x_indices)
-        miss = miss+1;
-        %[x_indices x0_indices]
-    end
+    misses(end+1) = miss;
+    means(end+1) = mean(iterations);
+    maxes(end+1) = max(iterations);
+    mins(end+1) = min(iterations);
+    
+    %disp("Number of misses: "+miss);
+    %disp("Total number of elements in x: "+p);
+    %disp("Mean number of cycles required for convergence: "+mean(iterations));
+    %disp("Min number of cycles required for convergence: "+min(iterations));
+    %disp("Max number of cycles required for convergence: "+max(iterations));
+    
+
 end
 
-disp("Number of misses: "+miss);
-disp("Total number of elements in x: "+p);
-disp("Mean number of cycles required for convergence: "+mean(iterations));
-disp("Min number of cycles required for convergence: "+min(iterations));
-disp("Max number of cycles required for convergence: "+max(iterations));
+figure(1)
+hold on
+xlabel("Dimension of y");
+ylabel("Number of misevaluation");
+title("Number of misevaluation in function of the dimension q of y");
+plot(Q,misses,"ko-");
+grid;
+hold off
+
+figure(2)
+hold on
+xlabel("Dimension of y");
+ylabel("Mean Convergence time");
+title("Mean convergence time in function of the dimension q of y");
+plot(Q,means,"ro-");
+grid;
+hold off
+
+figure(3)
+hold on
+xlabel("Dimension of y");
+ylabel("Maximum Convergence time");
+title("Maximum convergence time in function of the dimension q of y");
+plot(Q,maxes,"bo-");
+grid;
+hold off
+
+figure(4)
+hold on
+xlabel("Dimension of y");
+ylabel("Minimum Convergence time");
+title("Minimum convergence time in function of the dimension q of y");
+plot(Q,mins,"go-");
+grid;
+hold off
 
