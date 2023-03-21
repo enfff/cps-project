a0986f2a619803523ebc1a44fb5340c3065e482d
SimoneCarena <simonecarena2000@gmail.com>
2023-03-21 17:11:53 +0100

    added min max and mean cycles for convergence

--- project1/project1.m
+++ project1/project1.m
@@ -9,6 +9,8 @@ clc;
 format long
 
 miss = 0;
+outer_iterations = 20;
+iterations = zeros(outer_iterations,1);
 q = 10; %dimension of y
 lambda = 1e-1;
 p = 20; %dimension of x
@@ -19,7 +21,7 @@ nu = sigma^(2)*randn(q,1);
 delta = 1e-12;
 k=2; %numeber of non-null elements of x
 
-for i=1:20
+for i=1:outer_iterations
 
     C = randn(q,p);
     tau = norm(C,2)^(-2) - epsilon;
@@ -28,9 +30,9 @@ for i=1:20
 
     mask = randi([1 p],k,1); %generates k non-null indices
 
-    for i=1:p
-        if ~ismember(mask,i)
-            x(i) = 0;
+    for j=1:p
+        if ~ismember(mask,j)
+            x(j) = 0;
         end
     end
 
@@ -38,6 +40,8 @@ for i=1:20
     [x_zero_norm, x_indices] = zero_norm(x);
 
     while 1
+        iterations(i) = iterations(i)+1;
+        iterations(i)=iterations(i)+1;
         x_new = IST(x_0+tau*C'*(y-C*x_0),L);
         if norm(x_new - x_0,2)<delta
             break;
@@ -54,4 +58,7 @@ end
 
 disp("Number of misses: "+miss);
 disp("Total number of elements in x: "+p);
+disp("Mean number of cycles required for convergence: "+mean(iterations));
+disp("Min number of cycles required for convergence: "+min(iterations));
+disp("Max number of cycles required for convergence: "+max(iterations));
 
