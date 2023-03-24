3537129f9ed0015e2b07c0af31a1e7dad8d8aeb0
SimoneCarena <simonecarena2000@gmail.com>
2023-03-24 11:14:52 +0100

    task 3

---
+++ project1/project1_t3_1.m
@@ -0,0 +1,50 @@
+clear all;
+close all;
+clc;
+
+run realdata.m
+
+q = 6;
+p = 7;
+delta = 1e-12;
+lambda = 1e-1;
+L = lambda*ones(p,1);
+epsilon = 1e-8;
+tau = norm(D,2)^(-2) - epsilon;
+
+x = zeros(p,1);
+
+while 1
+    x_new = IST(x+tau*D'*(y-D*x),L);
+    if norm(x-x_new,2) < delta
+        break;
+    end
+    x = x_new;
+end
+
+x
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+q = 6;
+p = 7;
+delta = 1e-12;
+lambda = 1;
+epsilon = 1e-8;
+tau = norm(D,2)^(-2) - epsilon;
+L = lambda*tau*ones(p,1);
+D = normalize(D);
+
+x = zeros(p,1);
+
+while 1
+    x_new = IST(x+tau*D'*(y-D*x),L);
+    if norm(x-x_new,2) < delta
+        break;
+    end
+    x = x_new;
+end
+
+x
+
+
