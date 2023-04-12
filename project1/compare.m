e7f8c8efc38b4dcef61845692bcca4b41ff97da8
Francesco Paolo Carmone <enforcetitan@gmail.com>
2023-04-12 12:30:50 +0400

    code refactoring

--- project1/compare.m
+++ project1/compare.m
@@ -1,8 +1,9 @@
 function res = compare(v1,v2)
+    % Compares two vector componently-wise, returns 1 if they're equal,
+    % returns 0 if at least one component is different
     
     res = 1;
-    s = size(v1);
-    for i = 1:s(1)
+    for i = 1:length(v1)
         if v1(i)~=v2(i)
             res = 0;
         end
