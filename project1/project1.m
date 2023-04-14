bc2088e9cb640af8a6a0bc5d6dcc4ffe05f439ff
SimoneCarena <simonecarena2000@gmail.com>
2023-04-14 16:34:19 +0200

    small fiex for task 3

--- project1/project1.m
+++ project1/project1.m
@@ -2,9 +2,7 @@ close all;
 clear all;
 clc;
 
-%%%%%%%%
-%Task 1%
-%%%%%%%%
+%% Task 1
 
 format long
 
@@ -13,28 +11,28 @@ means = [];
 maxes = [];
 mins = [];
 
-Q = 10:50;
+Q = 10:50; % ranges on which to variate q
 for q=Q
     miss = 0;
     outer_iterations = 1000;
     iterations = zeros(outer_iterations,1);
     lambda = 1e-1;
-    p = 20; %dimension of x
-    L = lambda*ones(p,1); %vector of lambda 
-    epsilon = 1e-8; %precision (?)
-    sigma = 1e-2; %standard deviation
-    nu = sigma*randn(q,1);
+    p = 20; % dimension of x
+    L = lambda*ones(p,1); % vector of lambda 
+    epsilon = 1e-8; 
+    sigma = 1e-2; % standard deviation
+    nu = sigma*randn(q,1); % vettore di rumore
     delta = 1e-12;
-    k=2; %numeber of non-null elements of x
+    k=2; % number of non-null elements of x
 
     for i=1:outer_iterations
         C = randn(q,p);
         tau = norm(C,2)^(-2) - epsilon;
         
-        x = 1 + (2-1)*rand(p,1);
+        x = 1 + (2-1)*rand(p,1); % vettore x originale
         x_0 = zeros(p,1);
 
-        mask = randi([1 p],k,1); %generates k non-null indices
+        mask = randi([1 p],k,1); % generates k non-null indices
 
         for j=1:p
             if ~ismember(mask,j)
@@ -42,7 +40,7 @@ for q=Q
             end
         end
 
-        y = C*x + nu;
+        y = C*x + nu; % vettore di misurazioni y
         [x_zero_norm, x_indices] = zero_norm(x);
 
         while 1
@@ -89,7 +87,7 @@ hold on
 xlabel("Dimension of y");
 ylabel("Mean Convergence time");
 title("Mean convergence time in function of the dimension q of y");
-plot(Q,means,"ro-");
+plot(Q,means,"ko-");
 grid;
 hold off
 
@@ -98,7 +96,7 @@ hold on
 xlabel("Dimension of y");
 ylabel("Maximum Convergence time");
 title("Maximum convergence time in function of the dimension q of y");
-plot(Q,maxes,"bo-");
+plot(Q,maxes,"ko-");
 grid;
 hold off
 
@@ -107,7 +105,7 @@ hold on
 xlabel("Dimension of y");
 ylabel("Minimum Convergence time");
 title("Minimum convergence time in function of the dimension q of y");
-plot(Q,mins,"go-");
+plot(Q,mins,"ko-");
 grid;
 hold off
 
