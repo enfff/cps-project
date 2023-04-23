406b184a7045194e76f168907f0955ea6a3adb75
Francesco Paolo Carmone <enforcetitan@gmail.com>
2023-04-24 00:12:16 +0200

    updated plots

--- project1/project1_t5.m
+++ project1/project1_t5.m
@@ -3,8 +3,8 @@ close all;
 clc;
 
 load stochasticmatrices
+% load 'task5.mat'
 
-num_iterations = 1; % Per adesso solo una dopo da portare a  20
 n = 10;
 q = 20;
 h = 2; % sensor attacks
@@ -37,107 +37,127 @@ Z = zeros(n+q, q, T+1);             % Matrice di bookeeping
 
 
 % conservo i dati per i plot
+% rimangono fissi per ogni iterazione
 eigenvalues = {zeros(q, 1), zeros(q, 1), zeros(q, 1), zeros(q, 1)};
 essential_eigenvalues = zeros(1, 4);
-cvg_times = zeros(1, 4);
-misses = zeros(1, 4); 
-X_means = {zeros(n, 1), zeros(n, 1), zeros(n, 1), zeros(n, 1)};
-accuracies = zeros(1, 4);
+max_iterations = 20;
+
+% variano per ogni iterazione
+cvg_times = zeros(max_iterations, 4);
+misses = zeros(max_iterations, 4); % Quante volte sbaglia a stiamre il supporto
+accuracies = zeros(max_iterations, 4); % Quanto precisamente è stato stimato lo stato
+X_means = zeros(max_iterations, 4, n, 1); % Media del consenso sullo stato
 
 topologies = {Q1, Q2, Q3, Q4};
 
-for idx=1:4
-    Q = topologies{idx}; % Itera tra le matrici dentro topologies
-  
-    % Calcola essential eigenvalues per Q; ci serve dopo
-    tmp = abs(eig(Q));
-    eigenvalues{idx} = tmp;
-    essential_eigenvalues(idx) = tmp(end - 1);
-    clear tmp;
+%% 
 
-    for k=1:T
-        for i=1:q
-            arg = zeros(n+q, 1);
-            for j=1:q
-                arg = arg + Q(i,j)*Z(:, j, k);
-            end
-            Z(:, i, k+1) = IST(arg + tau*G(i,:)'*(y(i)-G(i,:)*Z(:, i, k)), L);
-        end
-        
-        if k > 1 % Salta la prima iterazione
-            diff = 0;
-            for i=1:q
-                diff = diff + norm(Z(:,i, k-1) - Z(:,i, k),2);
-            end
-    
-            if diff < delta % Stop criterion
-                break
-            end
-        end
-    end
-    
-    z_est = Z(:,:,k+1); % Sensors Consensus
-    X = z_est(1:n, :);
-    A = z_est(n+1:n+q, :);
-    
-    miss = 0;
-    for i=1:q
-        A(:, i) = prune_array(A(:, i), 0.2);
-    
-        [~, a_found_indices] = zero_norm(A(:, i));
-        [~, a_indices] = zero_norm(a);
-            
-        if ~compare(a_found_indices,a_indices)
-            miss = miss+1;
-        end
-    end
-    
-    X_mean = zeros(n, 1);
-    for i=1:q
-        X_mean = X_mean + X(:, i);
-    end
-    X_mean = X_mean / q;
-    
-    misses(idx) = miss; % # times a got badly estimated
-    X_means{idx} = X_mean;
-    accuracies(idx) = norm(x_tilde - X_mean)^2;
-    cvg_times(idx) = k+1;
+% La metto fuori perché dobbiamo calcolarlo una volta sola per tutte e
+% venti le iterazioni
+for idx=1:4
+Q = topologies{idx}; % Itera tra le matrici dentro topologies
+        % Calcola essential eigenvalues per Q; ci serve dopo
+        tmp = abs(eig(Q));
+        eigenvalues{idx} = tmp;
+        essential_eigenvalues(idx) = tmp(end - 1);
+        clear tmp; % tmp non ci serve più
 end
 
-%%
+% La ciccia sta qua
+for iteration=1:max_iterations
+    for idx=1:4
+        Q = topologies{idx}; % Itera tra le matrici dentro topologies
+    
+        for k=1:T
+            for i=1:q
+                arg = zeros(n+q, 1);
+                for j=1:q
+                    arg = arg + Q(i,j)*Z(:, j, k);
+                end
+                Z(:, i, k+1) = IST(arg + tau*G(i,:)'*(y(i)-G(i,:)*Z(:, i, k)), L);
+            end
+            
+            if k > 1 % Salta la prima iterazione
+                diff = 0;
+                for i=1:q
+                    diff = diff + norm(Z(:,i, k-1) - Z(:,i, k),2);
+                end
+        
+                if diff < delta % Stop criterion
+                    break
+                end
+            end
+        end
+        
+        z_est = Z(:,:,k+1); % Sensors Consensus
+        X = z_est(1:n, :);
+        A = z_est(n+1:n+q, :);
+        
+        miss = 0;
+        for i=1:q
+            A(:, i) = prune_array(A(:, i), 0.2);
+        
+            [~, a_found_indices] = zero_norm(A(:, i));
+            [~, a_indices] = zero_norm(a);
+                
+            if ~compare(a_found_indices,a_indices)
+                miss = miss+1;
+                return
+            end
+        end
+        
+        % Calcola il valor medio dello stato stimato da tutti i sensori
+        X_mean = zeros(n, 1);
+        for i=1:q
+            X_mean = X_mean + X(:, i);
+        end
+        X_mean = X_mean / q;
+        
+        misses(iteration, idx) = miss; % # times a got badly estimated
+        X_means(iteration, idx, :) = X_mean;
+        accuracies(iteration, idx) = norm(x_tilde - X_mean)^2;
+        cvg_times(iteration, idx) = k+1;
+    end
+end
 
+%% Plots
+
+iterations = linspace(1, max_iterations, max_iterations);
 figure(1)
+
 % Rate of attack detection: how many times the support of a is correctly
 % estimated, i.e., we identify the sensors under attack?
-subplot(2,2,1);
-plot(num_iterations, misses(1)/20, "ro");
-hold on;
-plot(num_iterations, misses(2)/20, "go");
-plot(num_iterations, misses(3)/20, "bo");
-plot(num_iterations, misses(4)/20, "ko");
+subplot(2,1,1);
 xlabel("Iterations");
 ylabel("Misses (%)");
-title("Rate of attack detection");
+title("Attack Detection Rate");
+axis padded
+hold on;
+plot(iterations, misses(:, 1)'/20, 'ro');
+plot(iterations, misses(:, 2)'/20, 'g.');
+plot(iterations, misses(:, 3)'/20, 'b^');
+plot(iterations, misses(:, 4)'/20, 'k*');
 grid, legend('$Q_1$','$Q_2$', '$Q_3$', '$Q_4$', 'Interpreter','latex');
 hold off;
 
 % Estimation accuracy
-% Poi cambia da punti in riga ('o' -> '-')
-subplot(2,2,2);
-plot(num_iterations, accuracies(1),"ro");
-hold on;
-plot(num_iterations, accuracies(2),"go");
-plot(num_iterations, accuracies(3),"bo");
-plot(num_iterations, accuracies(4),"ko");
+
+subplot(2,1,2);
 xlabel("Iterations");
-ylabel("Accuracy");
-title("Estimation accuracy");
-grid, legend('$Q_1$','$Q_2$', '$Q_3$', '$Q_3$', 'Interpreter','latex');
+ylabel("Accuracy (%)");
+title("State Estimation Accuracy");
+axis padded
+hold on;
+plot(iterations, 100 - accuracies(:, 1)', 'r-o');
+plot(iterations, 100 - accuracies(:, 2)', 'g-o');
+plot(iterations, 100 - accuracies(:, 3)', 'b-o');
+plot(iterations, 100 - accuracies(:, 4)', 'k-o');
+grid, legend('$Q_1$','$Q_2$', '$Q_3$', '$Q_4$', 'Interpreter','latex');
 hold off;
 
 % Convergence time (= number of iterations): can you see a relationship
 % between the essential spectral radius of Q and the convergence time?
-subplot(2,2,3);
+figure(2)
 domain = linspace(-2,20);
 hold on;
 plot(domain, essential_eigenvalues(1).^domain, "r-");
