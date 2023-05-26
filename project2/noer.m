9b1670f80789f64da1ec5c54558bad323c1cc4b7
Francesco Paolo Carmone <enforcetitan@gmail.com>
2023-05-26 12:09:40 +0300

    implemented neighborhood output estimation error

---
+++ project2/noer.m
@@ -0,0 +1,15 @@
+Neighborhood Output Estimation Error of the i-th agent
+% Ad adj matrix
+% G leader pinning matrix
+% i describes the i-th agent
+% yt0 leader output
+% yt vector containing all agents' outputs (column i-th element is
+% the i-th agent's state)
+
+function xi = nte(Ad, G, yt0, yt, i)
+    for j=1:6 % total agents number, i assumed N was a global variable
+        xi = xi + Ad(i, j)*(yt(j) - yt(i));
+    end
+
+    xi = xi + G(i, i)*(yt0 - yt(i));
+return
