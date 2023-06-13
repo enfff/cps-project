77d21c6d175da82b9d82daf6b63161afbd6a4dad
Francesco Paolo Carmone <enforcetitan@gmail.com>
2023-06-14 01:26:05 +0300

    added prefix to differentiate between tasks

--- project2/create_folder.m
+++ project2/create_folder.m
@@ -2,9 +2,14 @@
 % topology_3 / reference / Q10_R1
 % which contains the statuses and outputs calculated by using
 % Q = 10*eye(2), R = 5
+% topology_num  topology id
+% ref           reference type
+% Q             Q matrix
+% R             R matrix (scalar)
+% prefix        root folder prefix
 
 % example
-% topology_3
+% prefix/topology_3
 % | ->  Q10_R5
 % |     -> constant
 % |         -> status1.jpg
@@ -19,17 +24,16 @@
 % |         -> status2.jpg
 % |         -> output.jpg
 
-function [path] = create_folder(topology_num, ref,Q, R)
-    topology_name = 'topology_' + string(topology_num);
-    
+function [path] = create_folder(topology_num, ref, Q, R, prefix)
     if ispc % Genera path per windows
+        topology_name = prefix+'\'+'topology_' + string(topology_num);
         folder_name = 'Q' + string(Q(1,1)) + '_R' + string(R(1)) + '\' + ref;
         mkdir(topology_name, folder_name)
         path = topology_name + '\' + folder_name;        
     else % Genera path per Linux/MacOS
+        topology_name = prefix+'/'+'topology_' + string(topology_num);
         folder_name = 'Q' + string(Q(1,1)) + '_R' + string(R(1)) + '/' + ref;
         mkdir(topology_name, folder_name)
         path = topology_name + '/' + folder_name; 
     end 
-
 end
